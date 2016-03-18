31a3ee05cdbdca97d29b0ea0b035186efd4c4a68
timmi mcguire <timothea.mcguire@gmail.com>
2016-03-18 10:46:18 -0700

    make object accessible through console

--- build/js/app.js
+++ build/js/app.js
@@ -1,4 +1,34 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-// Hello dhfjkadhjfladjlsfka
+// var apiKey = require('./../.env').apiKey;
+// var getRepos = require('../js/back-end.js').getRepos;
+var apiKey  = "b5de66d7cf8fe05ae99cc443fa3664fdbb976bd6";
+
+
+$(document).ready(function(){
+    $('#weatherLocation').click(function(){
+      var user = $('#location').val();
+      $('#location').empty();
+
+      var url = 'https://api.github.com/users/' + user + '?access_token=' + apiKey;
+
+      $.get(url).then(function(response){
+        console.log(response);
+      }).fail(function(error){
+        console.log(error.responseJSON.message);
+      });
+    });
+
+  });
+
+
+
+
+//   getRepos = function(){
+//   $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
+//     console.log(response);
+//   }).fail(function(error){
+//     console.log(error.responseJSON.message);
+//   });
+// };
 
 },{}]},{},[1]);
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -1,1 +1,31 @@
-// Hello dhfjkadhjfladjlsfka
+// var apiKey = require('./../.env').apiKey;
+// var getRepos = require('../js/back-end.js').getRepos;
+var apiKey  = "b5de66d7cf8fe05ae99cc443fa3664fdbb976bd6";
+
+
+$(document).ready(function(){
+    $('#weatherLocation').click(function(){
+      var user = $('#location').val();
+      $('#location').empty();
+
+      var url = 'https://api.github.com/users/' + user + '?access_token=' + apiKey;
+
+      $.get(url).then(function(response){
+        console.log(response);
+      }).fail(function(error){
+        console.log(error.responseJSON.message);
+      });
+    });
+
+  });
+
+
+
+
+//   getRepos = function(){
+//   $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
+//     console.log(response);
+//   }).fail(function(error){
+//     console.log(error.responseJSON.message);
+//   });
+// };
